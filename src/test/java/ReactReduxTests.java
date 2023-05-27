fad5fccf5d3e3515988fa50a54ee1dcc99598ca8
Luis Garelli <luiscarlosgarelli@gmail.com>
2023-05-27 16:26:40 +0200

    added all tests in linear script mode, no page objects yet

--- src/test/java/ReactReduxTests.java
+++ src/test/java/ReactReduxTests.java
@@ -8,7 +8,6 @@ import org.testng.annotations.Test;
 
 public class ReactReduxTests {
 
-
   @BeforeMethod
   public void setup() {
     Configuration.browserSize = "1920x1080";
@@ -26,12 +25,26 @@ public class ReactReduxTests {
 
   @Test
   public void addNewPost() {
-
+    $(By.cssSelector("[href='#login']")).click();
+    $(By.cssSelector("[type='email']")).setValue("luiscarlosgarelli+test@gmail.com");
+    $(By.cssSelector("[type='password']")).setValue("toto");
+    $(By.cssSelector("[type='submit']")).click();
+    $(By.cssSelector("[href='#editor']")).click();
+    $$(By.cssSelector("input.form-control")).get(0).setValue("Test Title");
+    $$(By.cssSelector("input.form-control")).get(1).setValue("This article is about Ledger");
+    $$(By.cssSelector("input.form-control")).get(2).setValue("selenide ledger luis");
+    $(By.cssSelector("textarea.form-control")).setValue("Test Description Article bla bla bla");
+    $(By.cssSelector(".btn-primary")).click();
   }
 
   @Test
   public void addToFavorites() {
-
+    $(By.cssSelector("[href='#login']")).click();
+    $(By.cssSelector("[type='email']")).setValue("luiscarlosgarelli+test@gmail.com");
+    $(By.cssSelector("[type='password']")).setValue("toto");
+    $(By.cssSelector("[type='submit']")).click();
+    $(By.cssSelector(".nav-pills li+li")).click();
+    $(By.cssSelector(".btn-sm")).click();
   }
 
 }
